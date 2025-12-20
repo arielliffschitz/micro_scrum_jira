ffd46dd778882117d9289e6933109e70604921a6
Ariel Liffschitz <aliffschitz@gmail.com>
2025-12-20 13:05:48 +0100

    setup-gateway-with-eureka-routing

---
+++ msvc-gateway-server/src/test/java/com/ariel/mscrumjira/MsvcGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariel.mscrumjira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
